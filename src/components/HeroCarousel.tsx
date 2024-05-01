ad6d565a285fd281c0b4c4bd4fde943bf10d7810
Rudolph Christian Razul <razulchristian@gmail.com>
2024-05-01 16:50:48 +0800

    Build the HeroCarousel and add the automatic sliding

---
+++ src/components/HeroCarousel.tsx
@@ -0,0 +1,60 @@
+import { Item } from "@/pages/ShopPage";
+import { useEffect, useState } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+
+const HeroCarousel = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (!response.ok) {
+        throw new Error("Items unavailable.");
+      }
+      const data: Item[] = await response.json();
+      setItems(data);
+      setIsLoading(false);
+    };
+
+    fetchData().catch((error) =>
+      console.error("Fetching items failed:", error),
+    );
+  }, []);
+
+  return (
+    <Carousel
+      className="w-full"
+      plugins={[
+        Autoplay({
+          delay: 2000,
+        }),
+      ]}
+    >
+      <CarouselContent>
+        {items.map((item) => (
+          <CarouselItem
+            key={item.id}
+            className="flex items-center justify-center md:basis-1/2 lg:basis-1/3"
+          >
+            <div className="flex aspect-square items-center justify-center  p-1">
+              <img
+                src={item.image}
+                className="h-[300px] w-[200px] object-contain "
+              ></img>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+};
+
+export default HeroCarousel;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,18 @@
+import HeroCarousel from "@/components/HeroCarousel";
 import NavBar from "@/components/NavBar";
 
 const HomePage = () => {
   return (
     <>
       <NavBar />
-      <h1>HomePage</h1>
+      <div className="mt-8 flex h-full w-full flex-col gap-8 md:px-24 lg:px-32 xl:px-48 2xl:px-96">
+        <h1 className=" text-center text-xl font-semibold">
+          Welcome to the Future of Shopping Exceptional Quality, Unbeatable
+          Prices
+        </h1>
+
+        <HeroCarousel />
+      </div>
     </>
   );
 };

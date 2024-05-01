0b3d4caa5be4aa02b8e2d2b4908537393f3d2b62
Rudolph Christian Razul <razulchristian@gmail.com>
2024-05-01 17:21:04 +0800

    Some styling changes and added CTA buttons to homepage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,31 @@
 const Footer = () => {
   return (
-    <footer className="flex h-12 w-full items-center justify-center">
+    <footer className="flex h-12 w-full items-center justify-center py-2">
       <p className="font-md">
         Built by{" "}
-        <a href="https://github.com/christianrazul" className="font-semibold">
+        <a
+          href="https://github.com/christianrazul"
+          className="font-semibold"
+          target="_blank"
+        >
           Christian Razul{" "}
         </a>
         using{" "}
-        <a href="https://react.dev/" className="font-semibold">
+        <a href="https://react.dev/" className="font-semibold" target="_blank">
           ReactJS{", "}
         </a>
-        <a href="https://ui.shadcn.com/" className="font-semibold">
+        <a
+          href="https://ui.shadcn.com/"
+          className="font-semibold"
+          target="_blank"
+        >
           ShadCN{", "}
         </a>
-        <a href="https://fakestoreapi.com/" className="font-semibold">
+        <a
+          href="https://fakestoreapi.com/"
+          className="font-semibold"
+          target="_blank"
+        >
           FakeStoreAPI{" "}
         </a>
         with ❤️
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,15 @@
 import Footer from "@/components/Footer";
 import HeroCarousel from "@/components/HeroCarousel";
 import NavBar from "@/components/NavBar";
+import { Button } from "@/components/ui/button";
+import { Github, ShoppingBag } from "lucide-react";
+import { NavLink } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div className="flex h-screen flex-col">
       <NavBar />
-      <div className="flex h-full w-full flex-col items-center justify-center gap-8 md:px-24 lg:px-32 xl:px-48 2xl:px-96">
+      <div className="flex h-full flex-col items-center justify-center gap-12 md:px-24 lg:px-32 xl:px-48 2xl:px-96">
         <div className="flex flex-col items-center gap-4">
           <h1 className="text-center text-3xl font-semibold">
             Welcome to the Future of Shopping Exceptional Quality, Unbeatable
@@ -14,8 +17,22 @@ const HomePage = () => {
           </h1>
           <p>Check out some of our best sellers!</p>
         </div>
-
         <HeroCarousel />
+        <div className="flex gap-4">
+          <Button>
+            <ShoppingBag className="mr-2 h-4 w-4" />
+            <NavLink to="/shop">Shop Now</NavLink>
+          </Button>
+          <Button variant="secondary">
+            <Github className="mr-2 h-4 w-4" />
+            <a
+              href="https://github.com/christianrazul/shoppingcart"
+              target="_blank"
+            >
+              GitHub Repo
+            </a>
+          </Button>
+        </div>
       </div>
       <Footer />
     </div>

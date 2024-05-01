149bc8231f137aafac624260b258ed18c1378134
Rudolph Christian Razul <razulchristian@gmail.com>
2024-05-01 16:12:01 +0800

    Use shadcn table and add the total footer

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,30 +1,57 @@
 import NavBar from "@/components/NavBar";
 import { useCart } from "@/contexts/CartContext";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const CartPage = () => {
   const { cartItems } = useCart();
+
+  const totalPrice = cartItems.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+
   return (
-    <>
+    <div className="flex w-full flex-col justify-center">
       <NavBar />
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead>
-          <tr>
-            <th>Product</th>
-            <th>Quantity</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cartItems.map((item, index) => (
-            <tr key={index}>
-              <td>{item.title}</td>
-              <td>{item.quantity}</td>
-              <td>${item.price}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+      <div className="mt-8 md:px-24 lg:px-32 xl:px-48 2xl:px-96">
+        <Table className="w-full divide-y divide-gray-200 border">
+          <TableCaption>A list of your cart items.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Product</TableHead>
+              <TableHead>Quantity</TableHead>
+              <TableHead>Price</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {cartItems.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell className="flex items-center gap-4">
+                  <img src={item.image} className="h-10 w-10 object-contain" />
+                  {item.title}
+                </TableCell>
+                <TableCell>{item.quantity}</TableCell>
+                <TableCell>${item.price}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={2}>Total</TableCell>
+              <TableCell>${totalPrice.toFixed(2)}</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    </div>
   );
 };
 
